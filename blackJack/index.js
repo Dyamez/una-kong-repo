86ec9ba1d095f94400b242d645e3a21280f258d9
Dyamez <dyamez@outlook.com>
2023-03-20 23:14:44 -0700

    031923_4

--- blackJack/index.js
+++ blackJack/index.js
@@ -1,21 +1,41 @@
-let firstCard = 4
-let secondCard =5
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+//let firstCard = getRandomCard()
+//let secondCard =getRandomCard()
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messegeEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el") 
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random() * 13) + 1
+    if (randomNumber > 10){
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+     
+}
 //let sumEl = document.querySelector("#sum-el")
 //console.log(sum)
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard + secondCard]
+    let sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = ("Cards: " + cards[0] + " " + cards[1])
+    cardsEl.textContent = "Cards: " 
+    for ( i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = ("Sum: ", + sum)
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -30,13 +50,18 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    let card = 3
-    sum += card
-    startGame()
+    //console.log("Drawing a new card from the deck!")
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+    
+    //startGame()
     
 }
-
+/*
 let age = 22
 
 if (age <= 20) {
@@ -52,10 +77,11 @@ else {
 
 
 
-/*
+
 else if (age > 21) {
     console.log("Good Luck!")
 } else if (age === 21) {
     console.log("Welcome!")
 } 
 */
+ 
